a3d7e71e542a77ab17293f30ae51fa8e368d4e2b
Dipshan Adhikari <dipshanadh@gmail.com>
2023-11-14 19:10:39 +0545

    Add problem description

--- fib.ts
+++ fib.ts
@@ -1,4 +1,12 @@
-// memoization
+/*
+  Problem description:
+
+  Write a function `fib(n)` that takes in a number as an argument. 
+  The function should return the n-th number of the Fibonacci sequence.
+
+  The 1st and 2nd number of the sequence is 1.
+  To generate the next number of the sequence, we sum the previous two.
+*/
 
 const fib = (n: number, memo: Record<number, number> = {}): number => {
   if (n in memo) return memo[n];
